2fad8f9c7e906294568b413da2c62b8ba1a53e02
anggi <ortegacanillo76.1@gmail.com>
2025-09-24 23:07:24 +0800

    extend resource to cart service

--- src/app/services/cart-service.ts
+++ src/app/services/cart-service.ts
@@ -1,8 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ResourceService } from './resource-service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CartService {
-  
+export class CartService extends ResourceService {
+  constructor(private http: HttpClient) {
+    super(http);
+  }
 }
